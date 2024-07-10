a8be8a64dd4610b60a30cb1f961ec0b8b90a9b0d
doaaapdelhai <doaaapdelhai@gmail.com>
2024-07-10 15:09:35 +0300

    commit-fixed-videoCard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,9 +5,11 @@ import {VideoCard , ChannelCard} from './';
 
 
 
-const Videos = ({videos}) => {
+const Videos = ({videos , direction}) => {
+  if(!videos?.length) return "loding....";
+
   return(
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
     {videos.map((item , idx)=>(
       <Box key={idx}>
       {item.id.videoId && <VideoCard video={item}/>}
