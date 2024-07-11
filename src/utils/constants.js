4286c872918a9a65c3516e642e962a2b7c20941f
doaaapdelhai <doaaapdelhai@gmail.com>
2024-07-11 11:21:37 +0300

    commit-done-responsev

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,6 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'Coding', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
